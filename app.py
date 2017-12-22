d9f5bdcddc2f119e86fbcfcd3fdde40d54f0b196
Daniel Jauregui <djauregui@csumb.edu>
2017-12-22 21:33:55 +0000

    adding play of the week

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return render_template('index.html')
-    
+@app.route('/play')
+def playOfTheWeek():
+    return render_template('playOfTheWeek.html')
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(8080), debug=True)
