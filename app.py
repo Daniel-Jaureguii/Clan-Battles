5c75597598ef32e24cbbbb53647a1d86a17b30cd
Daniel Jauregui <djauregui@csumb.edu>
2017-12-23 00:23:02 +0000

    routed all files

--- app.py
+++ app.py
@@ -9,6 +9,15 @@ def hello_world():
 @app.route('/play')
 def playOfTheWeek():
     return render_template('playOfTheWeek.html')
+@app.route('/rankings')
+def rankings():
+    return render_template('rankings.html')
+@app.route('/freeAgents')
+def freeAgents():
+    return render_template('freeAgents.html')
+@app.route('/chat')
+def chat():
+    return render_template('chatRoom.html')
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=int(8080), debug=True)
